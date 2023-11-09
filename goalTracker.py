d89604ed68a57b555c15c3d2b7bbbc4b590fbc53
choan <45882049+sdcorlin@users.noreply.github.com>
2023-11-08 22:03:30 -0800

    Add files via upload

---
+++ goalTracker.py
@@ -0,0 +1,118 @@
+import time
+import requests
+import json
+import schedule
+from datetime import datetime
+
+
+favorite_team = "LAK"
+def updateScore(boxscore):
+    if boxscore['clock']['inIntermission'] == True:
+        print("Intermission! {} Remaining!".format(boxscore['clock']['timeRemaining']))
+        print("{}: {}\n{}: {}\n".format(boxscore['awayTeam']['abbrev'], boxscore['awayTeam']['score'], boxscore['homeTeam']['abbrev'], boxscore['homeTeam']['score']))
+        time.sleep(boxscore['clock']['secondsRemaining'])
+    else:
+        print("{}\nPeriod - {}\nTime - {}".format(boxscore['gameState'], boxscore['period'], boxscore['clock']['timeRemaining']))
+        print("{}: {}\n{}: {}\n".format(boxscore['awayTeam']['abbrev'], boxscore['awayTeam']['score'], boxscore['homeTeam']['abbrev'], boxscore['homeTeam']['score']))
+
+def live_game(game_url, home_team):
+    # Game States: PRE, LIVE, FINAL
+    currentGoals = 0
+    while 1:
+        boxscore = requests.get(game_url)
+        if boxscore.status_code == 200:
+            boxscore = json.loads(boxscore.text)
+            if boxscore['gameState'] == "FINAL":
+                updateScore(boxscore)
+                break
+            if boxscore[home_team]['score'] > currentGoals:
+                print("GOALLLL!")
+                currentGoals = boxscore[home_team]['score']
+            elif boxscore[home_team]['score'] < currentGoals:
+                print("Goal Disallowed!!!")
+                currentGoals = boxscore[home_team]['score']
+            updateScore(boxscore)
+            time.sleep(30)
+
+def waitToStart(startTime):
+    if datetime.utcnow() < datetime.strptime(startTime, "%Y-%m-%dT%H:%M:%SZ"):
+        print("Game hasn't started yet! Waiting to continue!")
+    while datetime.utcnow() < datetime.strptime(startTime, "%Y-%m-%dT%H:%M:%SZ"):
+        time.sleep(600)
+    
+def get_games():
+    game_id = 0
+    game_id, home_team = gamesToday()
+    if game_id!=0:
+        game_boxscore_url = "https://api-web.nhle.com/v1/gamecenter/{}/boxscore".format(game_id)
+        live_game(game_boxscore_url, home_team)
+    else:
+        print("No game tonight :(")
+
+def gamesToday():
+    url = "https://api-web.nhle.com/v1/score/now"
+    data = requests.get(url)
+    if data.status_code != 200:
+        print("API Request Failed!")
+        return 0, "NA"
+    todaysGames = json.loads(data.text)['games']
+    for game in todaysGames:
+        if game['homeTeam']['abbrev'] == favorite_team:
+            gameStart = game.get("startTimeUTC")
+            print("Game Tonight!! {} @ {} at {}".format(game['awayTeam']['abbrev'], game['homeTeam']['abbrev'], gameStart))
+            waitToStart(gameStart)
+            return game['id'], "homeTeam"
+        if game['awayTeam']['abbrev'] == favorite_team:
+            gameStart = game.get("startTimeUTC")
+            print("Game Tonight!! {} @ {} at {}".format(game['awayTeam']['abbrev'], game['homeTeam']['abbrev'], gameStart))
+            waitToStart(gameStart)
+            return game['id'], "awayTeam"
+    return 0, "NA"
+    
+schedule.every().day.at('09:00').do(get_games)
+
+if __name__ == "__main__":
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+# def living_room_start_callback():
+#     loop = asyncio.new_event_loop()
+#     asyncio.set_event_loop(loop)
+#     # loop.run_until_complete(living_room_game_start())
+#     loop.close()
+    
+# def bed_room_start_callback():
+#     loop = asyncio.new_event_loop()
+#     asyncio.set_event_loop(loop)
+#     # loop.run_until_complete(bed_room_game_start())
+#     loop.close()
+
+# async def living_room_game_start():
+#     try:
+#         await bulb_living_room.update()
+#         await bulb_living_room.set_hsv(180,100,50)
+#         await bulb_living_room.set_brightness(100)
+#         for i in range(5):
+#             await bulb_living_room.turn_off()
+#             time.sleep(0.5)
+#             await bulb_living_room.turn_on()
+#             time.sleep(1) 
+#         await bulb_living_room.set_color_temp(3000)
+#     except Exception:
+#         pass
+    
+# async def bed_room_game_start():
+#     try:
+#         await bulb_bed_room.update()
+#         await bulb_bed_room.set_hsv(180,100,50)
+#         await bulb_bed_room.set_brightness(100)
+#         for i in range(5):
+#             await bulb_bed_room.turn_off()
+#             time.sleep(0.5)
+#             await bulb_bed_room.turn_on()
+#             time.sleep(1) 
+#         await bulb_bed_room.set_color_temp(3000)
+#     except Exception:
+#         pass
